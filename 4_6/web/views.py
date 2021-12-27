968d32104e8d884b776800bd9c15bf86cb55a865
sadeghjamalzadeh <jamalzadehsadegh@gmail.com>
2021-12-26 23:16:55 -0800

    project 4_6

--- 4_6/web/views.py
+++ 4_6/web/views.py
@@ -5,5 +5,5 @@ def sad_msg(request, name):
     return HttpResponse(msg)
 
 def happy_msg(request, name, times):
-    msg = ''.join([f'You are great, {name} :) \n' for i in range(times)])
+    msg = '<br>'.join([f'You are great, {name} :)' for i in range(times)])
     return HttpResponse(msg)
